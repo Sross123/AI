4e6d96726a6169030ca35c984cfa6af047187ad3
Shashi <shashiross9@gmail.com>
2026-07-09 08:31:57 +0530

    Add binary files for chroma database and implement PDF loading and embedding in create_database.py

---
+++ RAG/create_database.py
@@ -0,0 +1,31 @@
+#load pdf
+#split into chunks
+#create the embaddings
+#store into chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_mistralai.embeddings import MistralAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = PyPDFLoader("document loader/deeplearning.pdf")
+docs = data.load()
+
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = splitter.split_documents(docs)
+
+embadding_modal = MistralAIEmbeddings()
+
+vectorStore = Chroma.from_documents(
+    documents=docs,
+    embedding=MistralAIEmbeddings(),
+    persist_directory="chroma_db"
+)
+
--- RAG/main.py
+++ RAG/main.py
@@ -7,32 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-file_path = Path(__file__).parent / "document loader/deeplearning.pdf"
-
-
-data = PyPDFLoader(str(file_path))
-
-docs = data.load()
-
 template = ChatPromptTemplate.from_messages(
     [("system", "you are an AI that summarizes the text"),
     ("human", "{data}")]
 )
-
-docs = data.load()
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-
-chunks = splitter.split_documents(docs)
-model = ChatMistralAI(model_name="mistral-small-2603")
-
-prompt = template.format_messages(data=chunks)
-
-response = model.invoke(prompt)
-
-print("\n\n")
-
-print(response.content)
